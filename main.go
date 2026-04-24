0d6e8262b77935f04b438563ee76ab73732c29f5
tiasz <teteurezende3@gmail.com>
2026-04-24 18:29:58 -0300

    Running a server and serving it a index file to '/' route

--- main.go
+++ main.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
+	http.HandleFunc("/", helloHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	http.ServeFile(w, req, "./static/index.html")
 }
